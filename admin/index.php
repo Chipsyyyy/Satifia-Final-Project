ec855add5426d525100c5dc089676812aac7f770
Chipsyyyy <202410116@fit.edu.ph>
2026-07-07 10:07:27 +0800

    ayoko na jusko

---
+++ admin/index.php
@@ -0,0 +1,15 @@
+<?php
+    session_start();
+    $title = "Dashboard";
+    $css_path = "../css/style.css";
+    $root_path = "../";
+    include('../include/header.php');
+?>
+
+<div class="admin-layout">
+    <main class="admin-main">
+        <h1>Dashboard</h1>
+    </main>
+</div>
+
+<?php include('../include/footer.php'); ?>
