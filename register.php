a6055a379a6215679f98290df8e256a9398a44f0
Kristine Joy Sarzuelo <202410494@fit.edu.ph>
2026-07-09 22:16:19 +0800

    Add register page

---
+++ register.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+    $title = "Create Account";
+    $active_nav = "";
+    include('include/header.php');
+    include('include/navigation.php');
+?>
+
+<div class="page-wrapper">
+    <div class="form-page">
+        <div class="form-card">
+
+        <h1 class="form-card-title">Create Account</h1>
+            <p class="form-card-sub">Join Satifia and start shopping.</p>
+
+        </div>
+    </div>
+</div>
+
+<?php include('include/footer.php'); ?>
