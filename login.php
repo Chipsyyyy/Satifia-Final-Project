53ca24b4c569c5cba0e27edc46bbc7f5b97a2825
Kristine Joy Sarzuelo <202410494@fit.edu.ph>
2026-07-10 00:34:06 +0800

    Add login form

--- login.php
+++ login.php
@@ -13,6 +13,21 @@
 
             <h1 class="form-card-title">Welcome Back</h1>
             <p class="form-card-sub">Sign in to your Satifia account.</p>
+        
+            <form action="process/checklogin.php" method="post" novalidate>
+                <div class="form-group">
+                    <label class="form-label" for="email">Email Address</label>
+                    <input type="email" class="form-control" name="email" id="email"
+                        placeholder="you@example.com">
+                </div>
+                <div class="form-group">
+                    <label class="form-label" for="password">Password</label>
+                    <input type="password" class="form-control" name="password" id="password"
+                        placeholder="Your password">
+                </div>
+                <button type="submit" name="submit" class="form-submit">Sign In</button>
+            </form>
+
         </div>
     </div>
 </div>
