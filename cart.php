d94a6a22afee95dd24d09a1ab77a4d70806a4a4c
fionyyrivas <202410121@fit.edu.ph>
2026-07-11 21:51:36 +0800

    cart page

---
+++ cart.php
@@ -0,0 +1,18 @@
+<?php
+    session_start();
+    $title = "Your Cart";
+    $active_nav = "";
+
+    include('include/header.php');
+    include('include/navigation.php');
+?>
+
+<div class="page-wrapper">
+    <div class="cart-page">
+
+        <h1 class="cart-page-title">Your Cart</h1>
+
+    </div>
+</div>
+
+<?php include('include/footer.php'); ?>
