1f0d4a22df5b5eec67c5c6d7273a3a59b0038f51
annrayeesq <esquiviasannraye@gmail.com>
2026-07-11 18:13:10 +0800

    add checkout form and order summary

--- checkout.php
+++ checkout.php
@@ -6,3 +6,22 @@ if(!isset($_SESSION['buyer_id'])) {
     exit();
 }
 
+if(!isset($_SESSION['cart']) || count($_SESSION['cart']) == 0) {
+    header('Location: cart.php');
+    exit();
+}
+
+$title = "Checkout";
+$active_nav = "";
+
+$cart = $_SESSION['cart'];
+$subtotal = 0;
+
+foreach($cart as $item) {
+    $price = (float) str_replace(',','', $item['price']);
+    $subtotal += $price * $item['qty'];
+}
+
+$shipping = ($subtotal >= 1500) ? 0 : 150;
+$total = $subtotal + $shipping;
+
