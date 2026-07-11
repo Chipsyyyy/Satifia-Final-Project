e09a08a8482500eeea741887ac4c16bed931a2d1
Kristine Joy Sarzuelo <202410494@fit.edu.ph>
2026-07-11 17:53:58 +0800

    Create confirmation email function

--- mailer.php
+++ mailer.php
@@ -7,4 +7,21 @@ require("phpmailer/src/Exception.php");
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
+function send_confirmation_email($to_email, $to_name, $token) {
+
+    $mail = new PHPMailer(true);
+
+    $mail->isSMTP();
+    $mail->Host       = "smtp.gmail.com";
+    $mail->Username   = "samnieves1206@gmail.com";
+    $mail->Password   = "syla ljqy vvap bwbx";
+    $mail->SMTPAuth   = true;
+    $mail->SMTPSecure = "tls";
+    $mail->Port       = 587;
+
+    $mail->setFrom("no-reply@satifia.com", "Satifia");
+    $mail->addAddress($to_email, $to_name);
+
+    $confirm_link = "http://localhost/Satifia/confirm.php?token=" . $token;
+}
 ?>
