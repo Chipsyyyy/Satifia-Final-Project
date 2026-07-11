8e118bd4572a1df5d8cc533911366d04c3a0c558
annrayeesq <esquiviasannraye@gmail.com>
2026-07-11 15:19:29 +0800

    Create about and team page

--- about.php
+++ about.php
@@ -2,3 +2,18 @@
 session_start();
 $title = "About Us";
 $active_nav = "about";
+include('include/header.php');
+include('include/navigation.php');
+?>
+
+<div class="page-wrapper">
+
+    <section class="about-hero">
+        <h1 class="about-hero-title">About <em>Satifia</em></h1>
+        <p class="about-hero-text">
+            We believe fashion should feel as good as it looks. Satifia was built on the idea that every woman deserves clothing that is thoughtfully made, effortlessly wearable, and uniquely her own.
+</p>
+</section>
+
+<div class="about-section">
+    <div class="about-mission">
