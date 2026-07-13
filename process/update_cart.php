4fb2a1abf83025826e757efb11ab6c7cbb0554aa
fionyyrivas <202410121@fit.edu.ph>
2026-07-13 23:50:34 +0800

    Add update cart quantity functionality

---
+++ process/update_cart.php
@@ -0,0 +1,16 @@
+<?php 
+    session_start();
+
+     if(isset($_POST['cart_key']) && isset($_POST['qty'])) {
+        $key = (int)$_POST['cart_key'];
+        $qty = (int)$_POST['qty'];
+
+        if($qty >= 1 && isset($_SESSION['cart'][$key])) {
+            $_SESSION['cart'][$key]['qty'] = $qty;
+        }
+    }
+
+    header('Location: ../cart.php');
+    exit();
+
+?>
