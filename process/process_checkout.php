79693c11046647f2213dc64c291a48dea5c9ef8a
annrayeesq <esquiviasannraye@gmail.com>
2026-07-13 20:10:01 +0800

    complete order process

--- process/process_checkout.php
+++ process/process_checkout.php
@@ -16,5 +16,43 @@ if(isset($_POST['submit'])) {
     
     $errors = array();
 
-    if
+    if(isset($_POST['submit'])) {
+        $recv_name = $_POST['recv_name'];
+        $recv_address = $_POST['recv_address'];
+        $recv_contact = $_POST['recv_contact'];
+        $recv_email = $_POST['recv_email'];
+        $payment_method = $_POST['payment_method'];
+
+        $errors = array();
+
+        if(empty(trim($recv_name))) {
+            $errors[] = "Please enter the recipient's name.";
+        }
+
+        if(empty(trim($recv_address))) {
+            $errors[] = "Please enter a delivery address.";
+        }
+
+        if(empty(trim($recv_contact))) {
+            $errors[] = "Please enter the recipient's contact information.";
+        }
+
+        if(empty(trim($recv_email))) {
+            $errors[] = "Please enter the recipient's email address for confirmation.";
+        }
+
+        if(!empty($errors)) {
+            $_SESSION['errors'] = $errors;
+            header('Location: ../checkout.php');
+            exit();
+        } 
+
+        $cart = $_SESSION['cart'];
+
+        
+
+
+
+        }
+    }
 }
