818ccbc81042bf4a6705baf586d07f475f7f8f9f
Kristine Joy Sarzuelo <202410494@fit.edu.ph>
2026-07-14 20:13:24 +0800

    Add site header with login, register, and cart links

--- include/header.php
+++ include/header.php
@@ -16,3 +16,29 @@
     <div class="announcement-bar">
         Free shipping on orders over &#8369;1,500 &nbsp;|&nbsp; New arrivals every week
     </div>
+
+    <header class="site-header">
+        <div class="header-inner">
+            <div style="width: 160px;"></div>
+
+            <a href="<?= $root_path ?? ''; ?>index.php" class="header-logo">
+            Sati<span>fia</span>
+            </a>
+
+            <div class="header-actions">
+                <?php if(isset($_SESSION['buyer_id'])): ?>
+                    <span style="font-size:13px;">Hi, <?= htmlspecialchars($_SESSION['buyer_name']); ?></span>
+                    <a href="<?= $root_path ?? ''; ?>process/logout.php">Logout</a>
+                <?php else: ?>
+                    <a href="<?= $root_path ?? ''; ?>login.php">Login</a>
+                    <a href="<?= $root_path ?? ''; ?>register.php">Register</a>
+                <?php endif; ?>
+                <a href="<?= $root_path ?? ''; ?>cart.php" class="cart-icon">
+                    &#128717; Cart
+                    <?php if(isset($_SESSION['cart']) && count($_SESSION['cart']) > 0): ?>
+                        <span class="cart-badge"><?= count($_SESSION['cart']); ?></span>
+                    <?php endif; ?>
+                </a>
+            </div>
+        </div>
+    </header>
