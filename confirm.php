978e9cf4aeefc09ef576d2b8b3a1a4e59063d018
Kristine Joy Sarzuelo <202410494@fit.edu.ph>
2026-07-14 15:22:57 +0800

    Add account confirmation logic

---
+++ confirm.php
@@ -0,0 +1,34 @@
+<?php
+    session_start();
+    require("db.php");
+
+    $message = "";
+    $type = "";
+
+    if(isset($_GET['token']) && !empty($_GET['token'])) {
+        $token = mysqli_real_escape_string($conn, $_GET['token']);
+
+        $result = mysqli_query($conn, "SELECT * FROM tblbuyers WHERE confirm_token = '$token'");
+
+        if(mysqli_num_rows($result) == 1) {
+            $buyer = mysqli_fetch_assoc($result);
+
+            if($buyer['is_confirmed'] == 1) {
+                $message = "Your account has already been confirmed. You may now log in.";
+                $type = "success";
+            } else {
+                mysqli_query($conn, "UPDATE tblbuyers SET is_confirmed = 1, confirm_token = NULL WHERE confirm_token = '$token'");
+                $message = "Your account has been successfully confirmed! You may now log in.";
+                $type = "success";
+            }
+        } else {
+            $message = "Invalid or expired confirmation link.";
+            $type = "danger";
+        }
+    } else {
+        $message = "No confirmation token provided.";
+        $type = "danger";
+    }
+
+    mysqli_close($conn);
+?>
